d708d8ab68ee6447d42a5ff2ef120b2ea37bec70
Zhang Zengbo <zengbo.zhang@gmail.com>
2020-05-21 15:27:27 +0800

    bip32 instead of hdkey

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 var bip39 = require('bip39');
-var hdkey = require('hdkey');
+var bip32 = require('bip32');
 var btcjs = require('bitcoinjs-lib');
 var ECPair = btcjs.ECPair;
 
@@ -7,15 +7,15 @@ function getBitCoinECPair(mnemonic, options)
 {
     var masterSeed = bip39.mnemonicToSeed(mnemonic);
 
-    var  masterNode = hdkey.fromMasterSeed(masterSeed);
+    var  masterNode = bip32.fromSeed(masterSeed);
 
     const btcPath = `m/44'/0'/0'/0/0`;
     var  node;
     if( options && options.path ) {
-        node = masterNode.derive(options.path);
+        node = masterNode.derivePath(options.path);
     }
     else {
-        node = masterNode.derive(btcPath); 
+        node = masterNode.derivePath(btcPath); 
     }
 
     return ECPair.fromPrivateKey(node.privateKey, {
